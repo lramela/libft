2340bba046eaa14ae541cb00f35b65011732d2ab
Lesego RAMELA <lramela@c4r3s2.wethinkcode.co.za>
2019-06-04 16:01:30 +0200

    ft_strstr.c

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: lramela <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 08:29:42 by lramela           #+#    #+#             */
-/*   Updated: 2019/05/30 08:14:17 by lramela          ###   ########.fr       */
+/*   Updated: 2019/06/04 16:00:07 by lramela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,24 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	if 
+	int i;
+	int j;
+	int len;
 
+	i = 0;
+	j = 0;
+	len = ft_strlen(needle);
+	if (len == 0)
+		return ((char *)haystack);
+	while(haystack[i] != '\0')
+	{
+		while (needle[j] == haystack[i + j])
+		{
+			if (j ==(len - i))
+				return ((char *)haystack + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
-
