7da7865ebb53b327d38df8166231b5c9874a8044
Lesego RAMELA <lramela@c4r3s2.wethinkcode.co.za>
2019-06-07 07:27:24 +0200

    ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lramela <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 14:10:04 by lramela           #+#    #+#             */
+/*   Updated: 2019/06/05 14:20:46 by lramela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	char *ptr_dst;
+	ptr_dst = dst;
+	while(n > 0);
+	{
+		if(*(unsigned char *) src != (unsigned char)c)
+		{
+			*ptr_dst = *(unsigned char *)src;
+			ptr_dst++;
+			src++;
+		}
+		else
+		{
+			*ptr_dst = *(unsigned char *)src;
+			ptr_dst++;
+			src++;
+			return (ptr_dst);
+		}
+		n--;
+	}
+	return (NULL);
+}
