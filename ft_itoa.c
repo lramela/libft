e6b2202935b57f6d8646894cbe2b133a4569e89c
Lesego RAMELA <lramela@c4r4s2.wethinkcode.co.za>
2019-06-18 14:26:05 +0200

    modified files

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lramela <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 11:05:21 by lramela           #+#    #+#             */
-/*   Updated: 2019/06/17 11:23:57 by lramela          ###   ########.fr       */
+/*   Updated: 2019/06/18 14:24:47 by lramela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,48 @@
 
 static	int		ft_int_len(int n)
 {
-	int len;
+    int len;
 
-	len = 0;
-	if (n == 0)
-		len++;
-	if (n < 0)
-	{
-		n *= -1;
-		len++;
-	}
-	return (len);
+    len = 0;
+    if (n == 0)
+        len++;
+    if (n < 0)
+    {
+        n *= -1;
+        len++;
+    }
+    while (n)
+    {
+        n /= 10;
+        len++;
+    }
+    return (len);
 }
 
-char		*ft_itoa(int n)
+char            *ft_itoa(int n)
 {
-	int		len;
-	char	*str;
+    int        len;
+    char    *str;
 
-	len = ft_int_len(n);
-	str = (char *)malloc(sizeof(char) * len + 1);
-	if (!str)
-		return (NULL);
-	if (n == -2147483648)
-		ft_strcpy(str, "-2147483648");
-	str[len] = '\0';
-	if (n == 0)
-		str[0] = '0';
-	if (n < 0)
-	{
-		str[0] = '=';
-		n *= -1;
-	}
-	while (n > 0)
-	{
-		str[len - 1] = '0' + (n % 10);
-		n = n / 10;
-		len--;
-	}
-	return (str);
+    len = ft_int_len(n);
+    str = (char *)malloc(sizeof(char)* len + 1);
+    if (!str)
+        return (NULL);
+    if (n == -2147483648)
+        ft_strcpy(str, "-2147483648");
+    str[len] = '\0';
+    if (n == 0)
+        str[0] = '0';
+    if (n < 0)
+    {
+        str[0] = '-';
+        n *= -1;
+    }
+    while (n > 0)
+    {
+        str[len - 1] = '0' + (n % 10);
+        n = n / 10;
+        len--;
+    }
+    return (str);
 }
-
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,37 +6,26 @@
 /*   By: lramela <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 07:31:07 by lramela           #+#    #+#             */
-/*   Updated: 2019/06/17 15:36:04 by lramela          ###   ########.fr       */
+/*   Updated: 2019/06/18 14:21:31 by lramela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+char    *ft_strtrim(char const *s)
 {
-	size_t 	i;
-	size_t 	end;
-	size_t 	start;
-	size_t 	len;
-	char	*str;
-
-	i = 0;
-	len = ft_strlen(s);
-	start = 0;
-	end = 1;
-	while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
-		start++;
-	while (s[len - end] == ' ' || s[len - end] == '\n' || s[len - end] == '\n')
-		end++;
-	len = len - end - start + 1;
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
-		return (NULL);
-	while (i < len)
-	{
-		str[i] = (char)s[start + i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+    size_t    start;
+    size_t    len;
+    
+    start = 0;
+    if (!s)
+        return (NULL);
+    while ((s[start] == ' ' || s[start] == '\n' || s[start] == '\t'))
+        start++;
+    if (s[start] == '\0')
+        return (ft_strdup(s + start));
+    len = ft_strlen(s) - 1;
+    while ((s[len] == ' ' || s[len] == '\t' || s[len] == '\n') && len > 0)
+        len--;
+    return (ft_strsub(s, start, len - start + 1));
 }
